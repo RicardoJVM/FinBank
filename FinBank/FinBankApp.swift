f65337d35eb813964b1007e0d62e304c018a894f
Ricardo Valencia <rvalencia@t2company.com.uy>
2026-08-05 10:59:10 -0300

    Initial Commit

---
+++ FinBank/FinBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinBankApp.swift
+//  FinBank
+//
+//  Created by Ricardo Valencia on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct FinBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
